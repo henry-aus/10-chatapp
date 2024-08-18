bd6af0ed6f135269566fb14b645a42bcf864930d
Tyr Chen <tyr.chen@gmail.com>
2024-08-18 11:30:22 -0700

    feature: process sse

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,36 @@
 const URL_BASE = 'http://localhost:6688/api';
+const SSE_URL = 'http://localhost:6687/events';
 
 const getUrlBase = () => {
   return URL_BASE;
 }
 
+const initSSE = (store) => {
+  let url = `${SSE_URL}?token=${store.state.token}`;
+  const sse = new EventSource(url);
+
+  sse.addEventListener("NewMessage", (e) => {
+    let data = JSON.parse(e.data);
+    console.log('message:', e.data);
+    delete data.event;
+    store.commit('addMessage', { channelId: data.chatId, message: data });
+  });
+
+  sse.onmessage = (event) => {
+    console.log('got event:', event);
+    // const data = JSON.parse(event.data);
+    // commit('addMessage', data);
+  };
+
+  sse.onerror = (error) => {
+    console.error('EventSource failed:', error);
+    sse.close();
+  };
+
+  return sse;
+}
+
 export {
-  getUrlBase
+  getUrlBase,
+  initSSE,
 };
